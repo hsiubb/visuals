96230c78902e079a1d0318e04d0e1fec0b3e2a9a
Unknown <479833352@qq.com>
2019-05-23 18:05:05 +0800

    add beam block

---
+++ src/js/beam.js
@@ -0,0 +1,125 @@
+let beam;
+(function() {
+	if('ontouchstart' in window) {
+		evnt_move = 'touchstart';
+		evnt_end = 'touchend';
+	} else {
+		evnt_move = 'mousemove';
+		evnt_end = 'mouseout';
+	}
+
+	const COLOR = ['#9ee', '#09f', '#f60', '#e99', '#e9e', '#ee9', '#99e', '#9e9'];
+	let lights = [];
+	let light_len = 20;
+	let globalSize, unitSize, stepSize;
+	let cvs = document.createElement('canvas');
+	let ctx = cvs.getContext('2d');
+	let ori = {};
+
+	function box_init(box) {
+		ori.x = box.offsetLeft;
+		ori.y = box.offsetTop;
+		globalSize = Math.min(box.offsetWidth, box.offsetHeight);
+		unitSize = globalSize * .1;
+		stepSize = unitSize * .1;
+
+		cvs.style.width = globalSize;
+		cvs.style.height = globalSize;
+		cvs.width = globalSize;
+		cvs.height = globalSize;
+
+		box.innerHTML = '';
+		box.appendChild(cvs);
+	}
+
+	class Beam {
+		constructor() {
+			this.reset();
+		}
+		reset() {
+			this.x = Math.floor(Math.random() * 9 + .5) * unitSize;
+			this.y = Math.floor(Math.random() * 9 + .5) * unitSize;
+			this.dir = Math.floor(Math.random() * 4);
+			this.step = 0;
+
+			this.count();
+		}
+		count() {
+			this.step++;
+			switch(this.dir) {
+				case 1:
+					this.tar_x = this.x;
+					this.tar_y = this.y - stepSize;
+					break;
+				case 2:
+					this.tar_x = this.x + stepSize;
+					this.tar_y = this.y;
+					break;
+				case 3:
+					this.tar_x = this.x;
+					this.tar_y = this.y + stepSize;
+					break;
+				default:
+					this.tar_x = this.x - stepSize;
+					this.tar_y = this.y;
+			}
+			if(this.step == 10) {
+				this.step = 0;
+				this.dir = (this.dir + 3 + Math.floor(Math.random() * 3)) % 4;
+			}
+		}
+		check() {
+			if (
+				this.x >= globalSize ||
+				this.y >= globalSize ||
+				this.x <= 0 ||
+				this.y <= 0
+			) {
+				this.reset();
+				if(lights.length < light_len) {
+					setTimeout(function() {
+						lights.push(new Beam());
+					}, 599);
+				}
+			}
+		}
+		update() {
+			ctx.save();
+				ctx.beginPath();
+					ctx.moveTo(this.x, this.y);
+					ctx.lineTo(this.tar_x, this.tar_y);
+					ctx.strokeStyle = '#09f';
+					ctx.stroke();
+				ctx.closePath();
+			ctx.restore();
+
+			this.x = this.tar_x;
+			this.y = this.tar_y;
+
+			this.count();
+			this.check();
+		}
+	}
+
+	beam = function(id) {
+		let box = document.getElementById(id);
+		box_init(box);
+
+		lights.push(new Beam());
+
+		update();
+	}
+
+	function update() {
+		ctx.fillStyle = 'rgba(48, 48, 48, .2)';
+		ctx.fillRect(0, 0, globalSize, globalSize);
+
+		lights.map(function(light) {
+			light.update();
+		});
+
+		window.requestAnimationFrame(update);
+	}
+}());
+
+beam('beam');
